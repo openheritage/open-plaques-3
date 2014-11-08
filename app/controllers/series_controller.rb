1bb869e4f128792fd8e03eb1cb3ef071df4650eb
Frankie Roberto <frankie@frankieroberto.com>
2014-11-08 23:53:50 +0000

    Rails 4 fix

--- app/controllers/series_controller.rb
+++ app/controllers/series_controller.rb
@@ -8,7 +8,7 @@ class SeriesController < ApplicationController
   end
 
   def show
-    @plaques = @series.plaques.paginate(:page => params[:page], :per_page => 20, :order => 'series_ref ASC') # Postgres -> NULLS LAST
+    @plaques = @series.plaques.order('series_ref asc').paginate(:page => params[:page], :per_page => 20) # Postgres -> NULLS LAST
     @mean = help.find_mean(@plaques)
     respond_to do |format|
       format.html # show.html.erb
