e6ddc249e2dc21cb890eb83d3465359458055453
Simon Harriyott <simon@harriyott.com>
2014-11-09 11:26:36 +0000

    Fixed user create submit params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
 
     if Rails.env == 'development' && User.count == 0
       @user.is_admin = true
@@ -33,4 +33,15 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+
+    def user_params
+      params.require(:user).permit(
+        :username,
+        :email,
+        :password,
+        :password_confirmation,
+      )
+    end
+	
 end
