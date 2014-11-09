bb75ca056b2bb4e9d27d1acd5f3543c0318bb7e2
Simon Harriyott <simon@harriyott.com>
2014-11-09 12:00:05 +0000

    Fixed people by first letter controller

--- app/controllers/people_by_index_controller.rb
+++ app/controllers/people_by_index_controller.rb
@@ -5,8 +5,7 @@ class PeopleByIndexController < ApplicationController
     if @index =~ /^[A-Z]$/
       redirect_to people_by_index_path(@index.downcase), :status => :moved_permanently
     elsif @index =~ /^[a-z]$/
-      @people = Person.find(:all, :conditions => {:surname_starts_with => @index, :personal_connections_count => 1..999999})  # Can't figure out how to do a NOT "0" expression.
-      @people.sort! { |a,b| a.surname.downcase <=> b.surname.downcase }
+      @people = Person.where(['surname_starts_with = ? and personal_connections_count > 0', @index]).to_a.sort! { |a,b| a.surname.downcase <=> b.surname.downcase }
       respond_to do |format|
         format.html
         format.kml {
