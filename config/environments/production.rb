14c30f11482e6e93c8ba4d0514c5ca06e55708fd
Frankie Roberto <frankie@frankieroberto.com>
2014-11-16 16:27:23 +0000

    production settings update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,31 +1,12 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
+
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
-  # Disable minification since it adds a *huge* amount of time to precompile.
-  # Anyway, gzip alone gets us about 70% of the benefits of minify+gzip.
-  config.assets.js_compressor = false
-  config.assets.css_compressor = false
+  config.serve_static_assets = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
@@ -33,68 +14,8 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # `config.assets.precompile` has moved to config/initializers/assets.rb
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
-
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Production mailer config
-  config.action_mailer.delivery_method = :postmark
-  config.to_prepare do
-    ActionMailer::Base.postmark_settings = {
-      api_key: ENV.fetch("POSTMARK_API_KEY")
-    }
-  end
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {
-    host: 'rails-starter.com',
-    protocol: 'https'
-  }
-  config.action_mailer.asset_host = "https://rails-starter.com"
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  # Send error email whenever uncaught exception occurs in production
-  config.middleware.use ExceptionNotification::Rack,
-    :email => {
-      :email_prefix => "[#{Rails.env.to_s.upcase}] ",
-      :sender_address => %Q{"RailsStarter errors" <support@rails-starter.com>},
-      :exception_recipients => %w(support@rails-starter.com),
-      :sections => %w(version request environment backtrace),
-      :background_sections => %w(version backtrace data),
-      :email_format => :html
-    }
 end
