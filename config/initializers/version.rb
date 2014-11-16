9385f457fd7beb7e1a29f2c55f585f5f5620271c
Frankie Roberto <frankie@frankieroberto.com>
2014-11-16 16:41:48 +0000

    Unneeded file

--- config/initializers/version.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.config.version = `git describe --always --tag`.chomp
-Rails.application.config.version_date = \
-  Date.parse(`git log -1 --format="%ad" --date=short`)\
-  rescue Time.now.to_date
