ff5dd05de466cc51ec09c789bc6d8456fd56671a
Frankie Roberto <frankie@frankieroberto.com>
2014-11-16 16:47:29 +0000

    Remove unused files

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :branch, ENV.fetch("CAPISTRANO_BRANCH", "master")
-
-server "yoursitegoeshere.com",
-       :user => "deployer",
-       :roles => %w(app backup cron db web)
--- config/deploy/staging.rb
+++
@@ -1,5 +0,0 @@
-set :branch, ENV.fetch("CAPISTRANO_BRANCH", "development")
-
-server "staging.yoursitegoeshere.com",
-       :user => "deployer",
-       :roles => %w(app backup cron db web)
