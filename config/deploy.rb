934dae4019229ebfe3e38581140ed54d329c6b0a
Frankie Roberto <frankie@frankieroberto.com>
2014-11-16 16:44:06 +0000

    Remove unneeded deploy file

--- config/deploy.rb
+++
@@ -1,6 +0,0 @@
-set :application, "rails-starter"
-set :repo_url, "git@github.com:55minutes/rails-starter.git"
-
-# Project-specific overrides go here.
-# For list of variables that can be customized, see:
-# https://github.com/55minutes/capistrano-fiftyfive/blob/master/lib/capistrano/tasks/defaults.rake
