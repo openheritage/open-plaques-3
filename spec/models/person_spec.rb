889ed1ce030f60b769e2da1842bc08320b201d88
Jez Nicholson <jez.nicholson@gmail.com>
2014-11-17 22:23:38 +0000

    more person tests

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -6,24 +6,34 @@ describe Person do
 
   	context 'regular name' do
   	   before do
-         @frankie = Person.new(name: 'Frankie')
+         @person = Person.new(name: 'Frankie')
        end
        it 'joins the title and name' do
-       	 expect(@frankie.full_name).to eq('Frankie')
+       	 expect(@person.full_name).to eq('Frankie')
        end
     end
 
-  	context 'titled person' do
+  	context 'a Baronet is a Sir' do
   	   before do
-         @frankie = Person.new(name: 'Frankie')
-         @confers_title = Role.new(name: 'Baronet', abbreviation: 'Bt')
-         @frankie.roles << @confers_title
+         @person = Person.new(name: 'Frankie')
+         @role = Role.new(name: 'Baronet')
+         @person.roles << @role
        end
        it 'joins the title and name' do
-       	 expect(@frankie.full_name).to eq('Sir Frankie')
+       	 expect(@person.full_name).to eq('Sir Frankie')
        end
     end
 
+  	context 'letters after your name' do
+  	   before do
+         @person = Person.new(name: 'Frankie')
+         @role = Role.new(name: 'Toodle', abbreviation: 'Td')
+         @person.roles << @role
+       end
+       it 'joins the title and name' do
+       	 expect(@person.full_name).to eq('Frankie')
+       end
+    end
   end
 
 end
