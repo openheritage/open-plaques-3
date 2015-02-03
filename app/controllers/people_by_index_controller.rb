44dd43bbe88f987b8595617f2115a267aa128dc1
Frankie Roberto <frankie@frankieroberto.com>
2015-02-03 09:28:36 +0000

    Preload the main photos

--- app/controllers/people_by_index_controller.rb
+++ app/controllers/people_by_index_controller.rb
@@ -4,12 +4,13 @@ class PeopleByIndexController < ApplicationController
     @index = params[:id]
     if @index =~ /^[A-Z]$/
       redirect_to people_by_index_path(@index.downcase), :status => :moved_permanently
+    
     elsif @index =~ /^[a-z]$/
       
       @people = Person
         .where(surname_starts_with: @index)
         .where('personal_connections_count > 0')
-        .preload(:personal_roles, :roles)
+        .preload(:personal_roles, :roles, :main_photo)
         .to_a.sort! { |a,b| a.surname.downcase <=> b.surname.downcase }
       
       respond_to do |format|
