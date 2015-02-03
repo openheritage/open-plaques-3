d0046d463707f6121cecbd4c3793e072de29da6d
Frankie Roberto <frankie@frankieroberto.com>
2015-02-03 08:10:49 +0000

    Preload the required associations
    (This avoids N+1 queries)

--- app/controllers/people_by_index_controller.rb
+++ app/controllers/people_by_index_controller.rb
@@ -5,7 +5,13 @@ class PeopleByIndexController < ApplicationController
     if @index =~ /^[A-Z]$/
       redirect_to people_by_index_path(@index.downcase), :status => :moved_permanently
     elsif @index =~ /^[a-z]$/
-      @people = Person.where(['surname_starts_with = ? and personal_connections_count > 0', @index]).to_a.sort! { |a,b| a.surname.downcase <=> b.surname.downcase }
+      
+      @people = Person
+        .where(surname_starts_with: @index)
+        .where('personal_connections_count > 0')
+        .preload(:personal_roles, :roles)
+        .to_a.sort! { |a,b| a.surname.downcase <=> b.surname.downcase }
+      
       respond_to do |format|
         format.html
         format.kml {
