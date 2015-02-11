e796e9aeff6dd3c95248b3fd70804b7eed222c30
Jez Nicholson <jez.nicholson@gmail.com>
2015-02-11 18:47:51 +0000

    spot some bots
    show the user agent so that we can identify which crawlers are reading
    us

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
+  around_filter :global_request_logging
 
   def authenticate_admin!
     raise UnAuthorised, "NotAuthorised" unless current_user.try(:is_admin?)
   end
 
+  def global_request_logging 
+    puts "USERAGENT: #{request.path} #{request.headers['HTTP_USER_AGENT']}"
+    begin 
+      yield 
+    ensure 
+#      puts "response_status: #{response.status}"
+    end 
+  end 
 
 end
