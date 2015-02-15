1294b58287dc5d7e4f4d46d30cff0341b9a78e90
Jez Nicholson <jez.nicholson@gmail.com>
2015-02-15 09:34:02 +0000

    limit series/plaques json to tiled calls only

--- app/controllers/series_plaques_controller.rb
+++ app/controllers/series_plaques_controller.rb
@@ -3,17 +3,16 @@ class SeriesPlaquesController < ApplicationController
   before_filter :find, :only => [:show]
 
   def show
+    @plaques = @series
     zoom = params[:zoom].to_i
     if zoom > 0
       x = params[:x].to_i
       y = params[:y].to_i
       @plaques = @series.plaques.tile(zoom, x, y, '')
-    else
-      @plaques = @series.plaques
     end
     respond_to do |format|
       format.json {
-        render :json => @plaques.as_json(Plaque.as_minimal_geojson)
+        render :json => @plaques.as_json()
       }
     end
   end
