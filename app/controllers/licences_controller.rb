1d372acc4ebcb7ad9a73f53220949cf7a5378593
Jez Nicholson <jez.nicholson@gmail.com>
2015-03-19 06:46:34 +0000

    licences, languages, and colours i18n

--- app/controllers/licences_controller.rb
+++ app/controllers/licences_controller.rb
@@ -6,6 +6,7 @@ class LicencesController < ApplicationController
 
   def index
     @licences = Licence.all
+    @licences = @licences.sort_by { |p| 1 - p.photos.count }
     respond_to do |format|
       format.html
       format.json { render :json => @licences }
