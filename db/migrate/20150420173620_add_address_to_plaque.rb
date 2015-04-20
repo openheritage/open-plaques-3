59dfecc068bc64818b08272fe19e13eee8be826e
Jez Nicholson <jez.nicholson@gmail.com>
2015-04-20 21:54:05 +0100

    populate failed

--- db/migrate/20150420173620_add_address_to_plaque.rb
+++ db/migrate/20150420173620_add_address_to_plaque.rb
@@ -3,10 +3,10 @@ class AddAddressToPlaque < ActiveRecord::Migration
     add_column :plaques, :address, :string
     add_column :plaques, :area_id, :integer
     say_with_time("Setting counter caches for existing organisations") do
-      Plaques.find_each do |p|
+      Plaque.all.each do |p|
         p.address = p.location.full_address if p.location
         p.area_id = p.location.area_id if p.location && p.location.area
-        o.save
+        p.save
       end
     end
   end
