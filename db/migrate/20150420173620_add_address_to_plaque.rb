7904a4af4bea8f5641371274785eaedd5bd6fddc
Jez Nicholson <jez.nicholson@gmail.com>
2015-04-22 17:48:13 +0100

    migration takes too long
    I wanted to set the plaque.address but the update is getting in the way

--- db/migrate/20150420173620_add_address_to_plaque.rb
+++ db/migrate/20150420173620_add_address_to_plaque.rb
@@ -2,12 +2,12 @@ class AddAddressToPlaque < ActiveRecord::Migration
   def change
     add_column :plaques, :address, :string
     add_column :plaques, :area_id, :integer
-    say_with_time("Setting addresses for existing plaques") do
-      Plaque.all.each do |p|
-        p.address = p.location.full_address if p.location
-        p.area_id = p.location.area_id if p.location && p.location.area
-        p.save
-      end
-    end
+#    say_with_time("Setting addresses for existing plaques") do
+#      Plaque.all.each do |p|
+#        p.address = p.location.full_address if p.location
+#        p.area_id = p.location.area_id if p.location && p.location.area
+#        p.save
+#      end
+#    end
   end
 end
