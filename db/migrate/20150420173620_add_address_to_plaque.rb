14832e39691c96f89820bd28a0c18b291ba4f287
Jez Nicholson <jez.nicholson@gmail.com>
2015-04-26 12:12:08 +0100

    find_each is more efficient...sql is even better

--- db/migrate/20150420173620_add_address_to_plaque.rb
+++ db/migrate/20150420173620_add_address_to_plaque.rb
@@ -3,7 +3,7 @@ class AddAddressToPlaque < ActiveRecord::Migration
     add_column :plaques, :address, :string
     add_column :plaques, :area_id, :integer
 #    say_with_time("Setting addresses for existing plaques") do
-#      Plaque.all.each do |p|
+#      Plaque.find_each do |p|
 #        p.address = p.location.full_address if p.location
 #        p.area_id = p.location.area_id if p.location && p.location.area
 #        p.save
