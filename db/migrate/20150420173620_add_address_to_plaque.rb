8bd6de79d307ca7466fcc7803650b7808bcddf41
Jez Nicholson <jez.nicholson@gmail.com>
2015-04-26 17:51:04 +0100

    superfast SQL
    hmmm, don’t use ActiveRecord if doing a data update. SQL is superfast
    in comparison

--- db/migrate/20150420173620_add_address_to_plaque.rb
+++ db/migrate/20150420173620_add_address_to_plaque.rb
@@ -2,12 +2,8 @@ class AddAddressToPlaque < ActiveRecord::Migration
   def change
     add_column :plaques, :address, :string
     add_column :plaques, :area_id, :integer
-#    say_with_time("Setting addresses for existing plaques") do
-#      Plaque.find_each do |p|
-#        p.address = p.location.full_address if p.location
-#        p.area_id = p.location.area_id if p.location && p.location.area
-#        p.save
-#      end
-#    end
+    say_with_time("Setting addresses for existing plaques") do
+      Plaque.connection.execute("update plaques p set area_id=l.area_id, address=l.name from locations l where p.location_id = l.id")
+    end
   end
 end
