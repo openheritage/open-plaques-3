8c2f349c9b97599191951562e257460319ecf2c9
Jez Nicholson <jez.nicholson@gmail.com>
2015-04-27 10:16:54 +0100

    a bit of brevity

--- app/controllers/sponsorships_controller.rb
+++ app/controllers/sponsorships_controller.rb
@@ -1,24 +1,17 @@
 class SponsorshipsController < ApplicationController
 
   before_filter :authenticate_admin!, :only => :destroy
-
-  before_filter :find_plaque, :only => [:new, :index]
-  before_filter :find_sponsorship, :only => [:destroy]
+  before_filter :find, :only => [:destroy]
   before_filter :list_organisations, :only => [:new, :index]
 
-  respond_to :json
-
   def destroy
-
-    @sponsorship = Sponsorship.find(params[:id])
     plaque = @sponsorship.plaque
-
     @sponsorship.destroy
-
     redirect_to plaque_sponsorships_path(plaque)
   end
 
   def new
+    @plaque = Plaque.find(params[:plaque_id])
     @sponsorship = @plaque.sponsorships.new
     render 'plaques/sponsorships/new'
   end
@@ -31,38 +24,23 @@ class SponsorshipsController < ApplicationController
   end
 
   def index
-    respond_to do |format|
-      format.html {
-        @sponsorship = @plaque.sponsorships.new
-        render 'plaques/sponsorships/new'
-      }
-      format.json {
-        render :json => @plaque.sponsorships.as_json
-      }
-    end    
+    new 
   end
 
   private
 
-  def sponsorship_params
-    params.require(:sponsorship).permit(:plaque_id, :organisation_id)
-  end
-
-
-    def find_plaque
-      if params[:plaque_id]
-        @plaque = Plaque.find(params[:plaque_id])
-      end
+    def sponsorship_params
+      params.require(:sponsorship).permit(:plaque_id, :organisation_id)
     end
-    
-    def find_sponsorship
+
+    def find
       if params[:id]
         @sponsorship = Sponsorship.find(params[:id])
       end
     end
     
     def list_organisations
-      @organisations = Organisation.order('name')
+      @organisations = Organisation.select(:id, :name).order('name')
     end
 
 end
