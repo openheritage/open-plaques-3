68baa9561f9b0a716ae111a83b8257bb7f775b1d
Jez Nicholson <jez.nicholson@gmail.com>
2015-05-18 18:06:12 +0100

    wasn't returning son for maps

--- app/controllers/organisation_plaques_controller.rb
+++ app/controllers/organisation_plaques_controller.rb
@@ -19,12 +19,12 @@ class OrganisationPlaquesController < ApplicationController
     respond_with @plaques do |format|
       format.html { render @plaques }
       format.json {
-        # used by the map, so careful if trying to alter
-        render :json => @plaques.as_json(
-        :only => [:id, :latitude, :longitude, :inscription],
-        :methods => [:title, :uri, :colour_name]
-        ) 
+        render :json => { 
+          type: 'FeatureCollection',
+          features: @plaques.as_json({:only => [:id, :latitude, :longitude, :inscription]})
+        }
       }
+      format.geojson { render :json => @plaques }
     end
   end
 
