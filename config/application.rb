1b9a7d536805d39dc8bae0023545aae4cc9f0e67
Jez Nicholson <jez.nicholson@gmail.com>
2015-09-28 07:35:23 +0100

    Heroic changes locale for everyone!!!
    eek. Change of plan. Heroic appears to be changing the locale for
    everyone!

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,8 @@ module OpenPlaques
     config.assets.version = '1.0'
 
  #   config.i18n.enforce_available_locales = false
-    config.i18n.available_locales = [:'en-GB', :fr, :en, :ru]
+#    config.i18n.available_locales = [:'en-GB', :fr, :en, :ru]
+    config.i18n.available_locales = [:'en-GB', :en]
     config.i18n.default_locale = :'en-GB'
     config.i18n.fallbacks =[:en]
 
