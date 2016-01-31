4fd797122518e7630c9dc97a82ee9673d2065031
Jez Nicholson <jez.nicholson@gmail.com>
2016-01-31 13:05:13 +0000

    plaques latest in son only

--- spec/controllers/plaques_latest_controller_spec.rb
+++ spec/controllers/plaques_latest_controller_spec.rb
@@ -2,16 +2,7 @@ require 'spec_helper'
 
 describe PlaquesLatestController do
   
-
   describe "#show" do
-      
-    context 'html' do 
-      before { get :show }
-      
-      it 'should render 200' do
-        expect(response).to be_success
-      end
-    end
 
     context 'json' do 
       before { get :show, format: :json }
