eed471d258b9659895eadb257ba61cee491d82ee
Jez Nicholson <jez.nicholson@gmail.com>
2016-07-22 15:41:33 +0100

    tests require mimetype

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,6 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+Mime::Type.register 'application/json', :geojson
 Mime::Type.register 'application/vnd.google-earth.kml+xml', :kml
 Mime::Type.register 'text/plain', :poi
 Mime::Type.register 'application/osm+xml', :osm
