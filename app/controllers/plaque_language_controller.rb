3bc3f6d842ab525839745f7f50dff3fcf2ad541a
Jez Nicholson <jez.nicholson@gmail.com>
2016-07-29 07:44:28 +0100

    better styled plaque editing

--- app/controllers/plaque_language_controller.rb
+++ app/controllers/plaque_language_controller.rb
@@ -1,7 +1,7 @@
 class PlaqueLanguageController < PlaqueDetailsController
 
   def edit
-    @languages = Language.all(:order => :name)
+    @languages = Language.all.order(name: :desc)
     render "plaques/language/edit"
   end
 
