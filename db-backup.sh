4e15731eafb565382baf9f47297da0135f418a9e
Jez Nicholson <jez.nicholson@gmail.com>
2016-10-27 08:31:13 +0100

    do a data dump

--- db-backup.sh
+++ db-backup.sh
@@ -1,3 +1,4 @@
 heroku pg:backups capture --app open-plaques-beta
 curl -o db/db.dump `heroku pg:backups --app open-plaques-beta public-url`
-pg_restore -d openplaques_20160611 db/db.dump
+createdb openplaques_20161026
+pg_restore -d openplaques_20161026 db/db.dump
