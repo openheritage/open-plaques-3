0d210051280737c19a402d5bbe026fb923557002
Jez Nicholson <jez.nicholson@gmail.com>
2016-11-16 10:08:34 +0000

    auto backup
    adjusts your database.yml

--- db-backup.sh
+++ db-backup.sh
@@ -1,4 +1,5 @@
-heroku pg:backups capture --app open-plaques-beta
-curl -o db/db.dump `heroku pg:backups --app open-plaques-beta public-url`
-createdb openplaques_20161026
-pg_restore -d openplaques_20161026 db/db.dump
+# heroku pg:backups capture --app open-plaques-beta
+# curl -o db/db.dump `heroku pg:backups --app open-plaques-beta public-url`
+# createdb openplaques_$(date +'%Y-%m-%d')
+# pg_restore -d openplaques_$(date +'%Y-%m-%d') db/db.dump
+sed -iE "s/openplaques_[0-9|-]*/openplaques_$(date +'%Y-%m-%d')/g" config/database.yml
