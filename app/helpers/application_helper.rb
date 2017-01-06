ed609ae74d463a8505ed5f1374068e7d86772a17
Jez Nicholson <jez.nicholson@gmail.com>
2017-01-06 17:28:16 +0000

    auto-erb from Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -152,8 +152,8 @@ end
 class CustomRender < Redcarpet::Render::HTML
   def paragraph(text)
     text.gsub!(/plaque ([0-9]+)/) { |match|
-      %(<a href="/plaques/#{match[7..-1]}">#{match}</a>)
+      %(<div class="col-xs-6 col-sm-4 col-md-3"><%= render Plaque.find(#{match[7..-1]}) %></div>)
     }
-    %(<p>#{text}</p>)
+    %(<div class="row">#{text}</div>)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,15 +2,19 @@
 # === Attributes
 # * +name+ - The name of the page
 # * +slug+ - An identifier for the organisation, usually equivalent to its name in lower case, with spaces replaced by underscores. Used in URLs.
+# * +strapline+ - A sub heading
 # * +body+ - The content
 class Page < ActiveRecord::Base
 
+  before_validation :make_slug_not_war
   validates_presence_of :name, :slug, :body
   validates_uniqueness_of :slug
   validates_format_of :slug, with: /\A[a-z\_]+\z/, message: "can only contain lowercase letters and underscores"
 
+  include ApplicationHelper
+
   def to_s
   	name
   end
-  
+
 end
