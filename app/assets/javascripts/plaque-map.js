84085ff0739cca3532e3d6723f290fb1e75b8bf3
Jez Nicholson <jez.nicholson@gmail.com>
2017-06-08 19:29:27 +0100

    also in geojson layer

--- app/assets/javascripts/plaque-map.js
+++ app/assets/javascripts/plaque-map.js
@@ -151,7 +151,7 @@ function initmap()
                 if (!plaques["'#"+plaque.id+"'"])
                 {
                   var plaque_description = '<div class="inscription">' + truncate(plaque.inscription, 255) + '</div><div class="info">' +
-                    '<a class="link" href="https://openplaques.org/plaques/' + plaque.id + '">Plaque ' + plaque.id + '</a>';
+                    '<a class="link" href="http://openplaques.org/plaques/' + plaque.id + '">Plaque ' + plaque.id + '</a>';
                   layer.bindPopup(plaque_description);
                   var plaque_icon = new L.DivIcon({ className: 'plaque-marker', html: '', iconSize : 16 });
                   layer.setIcon(plaque_icon);
