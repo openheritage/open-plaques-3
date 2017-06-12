c4c32be6f2950b4309045741b93bb44813eb357e
Jez Nicholson <jez.nicholson@gmail.com>
2017-06-12 15:08:54 +0100

    redirect if photo was linked to nothing

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -76,7 +76,7 @@ class PhotosController < ApplicationController
     @plaque = @photo.plaque
     @person = @photo.person
     @photo.destroy
-    redirect_to @plaque ? edit_plaque_path(@plaque) : edit_person_path(@person)
+    redirect_to @plaque ? edit_plaque_path(@plaque) : @person ? edit_person_path(@person) : photos_path()
   end
 
   protected
