a5145e226d59128fd6957a6b01b21dd6240bf0b7
Jez Nicholson <jez.nicholson@gmail.com>
2018-02-20 18:58:30 +0000

    prettier person/plaques page

--- app/controllers/person_plaques_controller.rb
+++ app/controllers/person_plaques_controller.rb
@@ -1,10 +1,10 @@
 class PersonPlaquesController < ApplicationController
 
   def show
-    person = Person.find(params[:person_id])
-    @plaques = person.plaques
+    @person = Person.find(params[:person_id])
+    @plaques = @person.plaques
     respond_to do |format|
-      format.html { render @plaques }
+      format.html { render "people/plaques/show" }
       format.json { render json: @plaques.as_json(
         only: [:id, :inscription, :latitude, :longitude, :is_accurate_geolocation],
         methods: [:title, :uri, :colour_name]
