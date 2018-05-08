67f05cf29657e5844024b3696eb643574acdb0f6
Jez Nicholson <jez.nicholson@gmail.com>
2018-05-08 22:29:30 +0200

    place group order
    fixes #132

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,10 +3,22 @@ describe Role, type: :model do
     expect(create(:role)).to be_valid
   end
   describe '#animal?' do
-    let (:role) { build :role }
     context 'with no content' do
-      it 'is not' do
+      let (:role) { build :role }
+      it 'is not an animal' do
         expect(role.animal?).to be_falsey
+      end
+    end
+    context 'a person' do
+      let (:a_person) { build :role, role_type: 'person' }
+      it 'is not an animal' do
+        expect(a_person.animal?).to be_falsey
+      end
+    end
+    context 'a duck' do
+      let (:a_duck) { build :role, role_type: 'animal' }
+      it 'is an animal' do
+        expect(a_duck.animal?).to be_truthy
       end
     end
   end
