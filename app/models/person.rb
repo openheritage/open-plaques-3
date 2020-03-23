98919495bd343ef97764df986fa0b2ca5cbf84cd
Jez Nicholson <jez.nicholson@gmail.com>
2020-03-23 17:24:42 +0000

    protect

--- app/models/person.rb
+++ app/models/person.rb
@@ -373,8 +373,8 @@ class Person < ApplicationRecord
 
   def siblings
     siblings = []
-    father&.children.each { |child| siblings << child if child != self }
-    mother&.children.each { |child| siblings << child if child != self }
+    father&.children&.each { |child| siblings << child if child != self }
+    mother&.children&.each { |child| siblings << child if child != self }
     siblings.uniq.sort! { |a, b| a.born_on ? a.born_on : 0 <=> b.born_on ? b.born_on : 0 }
   end
 
