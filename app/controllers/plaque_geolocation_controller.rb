f021894debeab3eb1607d60daa6a373a60d1ca6a
Jez Nicholson <jez.nicholson@gmail.com>
2020-04-06 08:26:28 +0100

    sort areas alphabetically

--- app/controllers/plaque_geolocation_controller.rb
+++ app/controllers/plaque_geolocation_controller.rb
@@ -1,7 +1,7 @@
 # geolocate a plaque
 class PlaqueGeolocationController < PlaqueDetailsController
   def edit
-    @areas = Area.preload(:country)
+    @areas = Area.preload(:country).alphabetically
     render 'plaques/geolocation/edit'
   end
 end
