4e386ae009d158489fc65dd32fbbce0a187e046b
Jez Nicholson <jez.nicholson@gmail.com>
2021-05-18 18:05:18 +0100

    refactor

--- storage/dump.rb
+++ storage/dump.rb
@@ -1,12 +1,10 @@
 country = Country.find_by_name('United Kingdom')
 Dir.mkdir("/app/storage/#{country.name.downcase}") unless File.exists?("/app/storage/#{country.name.downcase}")
 country.areas.each do |area|
-  Dir.mkdir("/app/storage/#{country.name.downcase}/#{area.to_param}") unless File.exists?("/app/storage/#{country.name.downcase}/#{area.to_param}")
+  Dir.mkdir("/app/storage/#{country.name.downcase}/#{area.to_param}") unless File.exist?("/app/storage/#{country.name.downcase}/#{area.to_param}")
   area.plaques.find_in_batches do |group|
     group.each do |plaque|
-      if plaque.geolocated?
-        File.write("/app/storage/#{country.name.downcase}/#{area.to_param}/#{plaque.to_param}.json", plaque.to_json)
-      end
+      File.write("/app/storage/#{country.name.downcase}/#{area.to_param}/#{plaque.to_param}.json", plaque.to_json) if plaque.geolocated?
     end
   end
 end
